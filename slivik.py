52e49682df962c31f6e6fda2719d3561ebc11b11
jem1ky <helyst55455@gmail.com>
2024-12-03 15:41:10 +0300

    Update slivik.py

--- slivik.py
+++ slivik.py
@@ -20,13 +20,16 @@ class Slivik:
             self.last_element = new_Node
         self.size += 1
 
+    def recursive_str(self, node):
+        if node is None:
+            return ""
+        next_str = self.recursive_str(node.next)
+        if next_str:  # Если есть следующий элемент, добавляем запятую
+            return str(node.value) + ", " + next_str
+        return str(node.value)
+
     def __str__(self):
-        current = self.first_element
-        result = []
-        while current:
-            result.append(current.value)
-            current = current.next
-        return str(result)
+        return "[" + self.recursive_str(self.first_element) + "]"
 
     def get(self, index):
         if index < 0 or index >= self.size:
@@ -35,6 +38,26 @@ class Slivik:
         for _ in range(index):
             current = current.next
         return current.value
+    
+    def append(self, value):
+        new_node = self.Node(value)
+        if not self.first_element:
+            self.first_element = new_node
+            return
+        current = self.first_element
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def sum_indices(self):
+        current = self.first_element
+        index_sum = 0
+        index = 0
+        while current:
+            index_sum += index * current.value
+            current = current.next
+            index += 1
+        return index_sum
 
     def remove(self, index):
         if index < 0 or index >= self.size:
@@ -63,8 +86,7 @@ myarray.append(1)
 myarray.append(3)
 myarray.append(2)
 print(myarray)  # [1, 3, 2]
-print(myarray.get(1))  # 3
-myarray.remove(1)
+print(myarray)  # 3
+#myarray.remove(1)
 print(myarray)  # [1, 2]
-myarray.sort()
-print(myarray)  # [1, 2]
+# [1, 2]
